620ca057a9c0ace2875e7a1a08cce2258525d4d4
Venkatesan R <45921124+registerme1963@users.noreply.github.com>
2024-03-28 22:53:03 +0530

    Release Candidate 1.0
    Release Candidate 1.0

--- amd/src/commands.js
+++ amd/src/commands.js
@@ -58,6 +58,7 @@ export const getSetup = async() => {
         buttonImage,
     ] = await Promise.all([
                 getString('ketcherButtonNameTitle', component),
+                getString('ketcherButtonNameTitle', component),
                 getButtonImage('icon', component),
             ]);
 
